86cf024c6e635085f61539fef3680d65fde1c2fa
momodooly <momodooly@naver.com>
2024-10-13 04:01:13 +0900

    feat: loan 다국어 처리

--- src/main/java/com/kb/loan/mapper/LoanMapper.java
+++ src/main/java/com/kb/loan/mapper/LoanMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 public interface LoanMapper {
-    List<LoanDTO> getLoanList();
-    LoanDTO getLoanDetail(@Param("lno") Long lno);
+    List<LoanDTO> getLoanList(@Param("lan") String lan);
+    LoanDTO getLoanDetail(@Param("lno") Long lno, @Param("lan") String lan);
+    List<LoanDTO> getTopLoans(@Param("lan") String lan);
 }
